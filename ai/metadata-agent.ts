db9dd6ac30a0946cabe2a67392fdcb7d27b4ed32
kurodenjiro <kurodenjiro1@gmail.com>
2024-11-30 00:37:15 +0700

    update

--- ai/metadata-agent.ts
+++ ai/metadata-agent.ts
@@ -8,7 +8,7 @@ export async function ExtractRustMetadata({ sourceCode, account, methods }: any)
     system:
       `You are a rust developer.  ` +
       `When the user gives the source code and method. Provide your response as a JSON object with the following schema: ` +
-      ` returns [{ account:${account}, method: ${methods}  , description : description with method 100 words limit , show args if it has args: [ name: Argument name , type : data types ,description }} ]`,
+      ` returns [{ account:${account}, method: ${methods}  , description : description with method 100 words limit , args <only show if it has args>: [ name: Argument name , type : data types ,description }} ]   `,
     prompt: `Your response will not be in Markdown format, only JSON.Here is the source code : ${sourceCode} , method : ${methods}  `,
   });
   return text;
